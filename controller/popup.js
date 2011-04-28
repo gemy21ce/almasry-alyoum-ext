b0b960db89435acde10da4669c75b85dca26e8c1
gemy21ce@gmail.com <gemy21ce@gmail.com@16bb3918-c630-06de-37f5-c4d2cbef16c4>
2011-04-28 15:09:33 +0000

    new loading img.

--- controller/popup.js
+++ controller/popup.js
@@ -86,7 +86,7 @@ var ReaderPOPUP={
         $("#tabs-content").show();
         ReaderPOPUP.setCurrentTab(id);
         if(!window.localStorage['rss-cat-'+id]){
-            $("#tabs-content").html('<center><br/><br/><br/><br/><br/><img align="center" src="images/loading.gif"/></center>');
+            $("#tabs-content").html('<center><br/><br/><br/><br/><br/><img align="center" src="images/elmasry_loader.gif"/></center>');
             ReaderPOPUP.openTimeout=window.setTimeout(function(){
                 ReaderPOPUP.openCategory(id);
             }, 1000);
