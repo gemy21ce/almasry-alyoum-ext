2106b50320b9eb228a3959e30d8be7381b7596e2
gemy21ce@gmail.com <gemy21ce@gmail.com@16bb3918-c630-06de-37f5-c4d2cbef16c4>
2011-05-08 14:22:23 +0000

    option page style

--- controller/data.js
+++ controller/data.js
@@ -69,30 +69,6 @@ var data={
         category:'news'
     },{
         id:9,
-        title:'أسواق وشركات',
-        url:'http://www.almasryalyoum.com/rss_feed_term/8/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:10,
-        title:'استثمار',
-        url:'http://www.almasryalyoum.com/rss_feed_term/9/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:11,
-        title:'البترول والطاقة',
-        url:'http://www.almasryalyoum.com/rss_feed_term/10/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:12,
         title:'البورصة',
         url:'http://www.almasryalyoum.com/rss_feed_term/11/rss.xml',
         unreaditems:0,
@@ -100,23 +76,7 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:13,
-        title:'بنوك وتأمين',
-        url:'http://www.almasryalyoum.com/rss_feed_term/12/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:14,
-        title:'تحليل',
-        url:'http://www.almasryalyoum.com/rss_feed_term/13/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:15,
+        id:10,
         title:'الرياضة',
         url:'http://www.almasryalyoum.com/rss_feed_term/14/rss.xml',
         unreaditems:0,
@@ -124,23 +84,7 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:16,
-        title:'العالم',
-        url:'http://www.almasryalyoum.com/rss_feed_term/15/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:17,
-        title:'الشارع الرياضي',
-        url:'http://www.almasryalyoum.com/rss_feed_term/16/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:18,
+        id:11,
         title:'كرة عالمية',
         url:'http://www.almasryalyoum.com/rss_feed_term/17/rss.xml',
         unreaditems:0,
@@ -148,7 +92,7 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:19,
+        id:12,
         title:'كرة عربية',
         url:'http://www.almasryalyoum.com/rss_feed_term/18/rss.xml',
         unreaditems:0,
@@ -156,7 +100,7 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:20,
+        id:12,
         title:'كرة مصرية',
         url:'http://www.almasryalyoum.com/rss_feed_term/19/rss.xml',
         unreaditems:0,
@@ -164,39 +108,7 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:21,
-        title:'العقارات اليوم',
-        url:'http://www.almasryalyoum.com/rss_feed_term/20/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:22,
-        title:'اعلانات مبوبة',
-        url:'http://www.almasryalyoum.com/rss_feed_term/21/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:23,
-        title:'تحليل',
-        url:'http://www.almasryalyoum.com/rss_feed_term/22/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:24,
-        title:'حديث',
-        url:'http://www.almasryalyoum.com/rss_feed_term/23/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:25,
+        id:13,
         title:'علوم وتكنولوجيا',
         url:'http://www.almasryalyoum.com/rss_feed_term/24/rss.xml',
         unreaditems:0,
@@ -204,15 +116,7 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:26,
-        title:'برامج وألعاب',
-        url:'http://www.almasryalyoum.com/rss_feed_term/25/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:27,
+        id:14,
         title:'طب وصحة',
         url:'http://www.almasryalyoum.com/rss_feed_term/26/rss.xml',
         unreaditems:0,
@@ -220,23 +124,7 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:28,
-        title:'علوم وفضاء',
-        url:'http://www.almasryalyoum.com/rss_feed_term/27/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:29,
-        title:'كمبيوتر وانترنت',
-        url:'http://www.almasryalyoum.com/rss_feed_term/28/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:30,
+        id:15,
         title:'موبايل واتصالات',
         url:'http://www.almasryalyoum.com/rss_feed_term/29/rss.xml',
         unreaditems:0,
@@ -244,7 +132,7 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:31,
+        id:16,
         title:'فن وثقافة',
         url:'http://www.almasryalyoum.com/rss_feed_term/30/rss.xml',
         unreaditems:0,
@@ -252,7 +140,7 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:32,
+        id:17,
         title:'الناشر',
         url:'http://www.almasryalyoum.com/rss_feed_term/31/rss.xml',
         unreaditems:0,
@@ -260,7 +148,7 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:33,
+        id:18,
         title:'تلفزيون',
         url:'http://www.almasryalyoum.com/rss_feed_term/32/rss.xml',
         unreaditems:0,
@@ -268,7 +156,7 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:34,
+        id:19,
         title:'سينما',
         url:'http://www.almasryalyoum.com/rss_feed_term/33/rss.xml',
         unreaditems:0,
@@ -276,7 +164,7 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:35,
+        id:20,
         title:'فنون',
         url:'http://www.almasryalyoum.com/rss_feed_term/34/rss.xml',
         unreaditems:0,
@@ -284,15 +172,7 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:36,
-        title:'كتب',
-        url:'http://www.almasryalyoum.com/rss_feed_term/35/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:37,
+        id:21,
         title:'ملفات خاصة',
         url:'http://www.almasryalyoum.com/rss_feed_term/36/rss.xml',
         unreaditems:0,
@@ -300,7 +180,7 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:31,
+        id:22,
         title:'تحقيقات',
         url:'http://www.almasryalyoum.com/rss_feed_term/37/rss.xml',
         unreaditems:0,
@@ -308,7 +188,7 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:38,
+        id:23,
         title:'حوارات',
         url:'http://www.almasryalyoum.com/rss_feed_term/38/rss.xml',
         unreaditems:0,
@@ -316,7 +196,7 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:39,
+        id:24,
         title:'ملفات هامه',
         url:'http://www.almasryalyoum.com/rss_feed_term/39/rss.xml',
         unreaditems:0,
@@ -324,23 +204,7 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:40,
-        title:'قالوا',
-        url:'http://www.almasryalyoum.com/rss_feed_term/3037/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:41,
-        title:'حكايات مصريه',
-        url:'http://www.almasryalyoum.com/rss_feed_term/3038/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:42,
+        id:25,
         title:'الأخيرة',
         url:'http://www.almasryalyoum.com/rss_feed_term/5238/rss.xml',
         unreaditems:0,
@@ -348,89 +212,9 @@ var data={
         lastItemPubDate:0,
         category:'news'
     },{
-        id:43,
+        id:26,
         title:'تقارير المراسلين',
         url:'http://www.almasryalyoum.com/rss_feed_term/40703/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:44,
-        title:'ثورة الحرية',
-        url:'http://www.almasryalyoum.com/rss_feed_term/98554/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:45,
-        title:'حوارات الثورة',
-        url:'http://www.almasryalyoum.com/rss_feed_term/98556/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:46,
-        title:'الإعلام والثورة',
-        url:'http://www.almasryalyoum.com/rss_feed_term/98557/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:47,
-        title:'وجوه من الثورة',
-        url:'http://www.almasryalyoum.com/rss_feed_term/98558/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:48,
-        title:'الناس والثورة',
-        url:'http://www.almasryalyoum.com/rss_feed_term/98559/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:49,
-        title:'الثورة في أيام',
-        url:'http://www.almasryalyoum.com/rss_feed_term/98560/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:50,
-        title:'لاعبون ومواقف',
-        url:'http://www.almasryalyoum.com/rss_feed_term/98561/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:51,
-        title:'شهداء',
-        url:'http://www.almasryalyoum.com/rss_feed_term/98562/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:52,
-        title:'وثائق هامة',
-        url:'http://www.almasryalyoum.com/rss_feed_term/98563/rss.xml',
-        unreaditems:0,
-        active:false,
-        lastItemPubDate:0,
-        category:'news'
-    },{
-        id:53,
-        title:'أخبار الثورة',
-        url:'http://www.almasryalyoum.com/rss_feed_term/100733/rss.xml',
         unreaditems:0,
         active:false,
         lastItemPubDate:0,
@@ -438,7 +222,7 @@ var data={
     }],
     mutlimedia:{
         video:{
-            id:54,
+            id:27,
             title:'فيديو',
             url:'http://www.almasryalyoum.com/rss_feed_video/rss.xml',
             unreaditems:0,
@@ -447,7 +231,7 @@ var data={
             category:'thumb'
         },
         car:{
-            id:55,
+            id:28,
             title:'كاريكاتير',
             url:'http://www.almasryalyoum.com/rss_feed_caricature/rss.xml',
             unreaditems:0,
